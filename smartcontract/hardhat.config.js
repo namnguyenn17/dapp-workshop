7a84df6b86087e96fd673bc1c2d780c9d7073c6c
Nam Nguyen <ducnam1719@gmail.com>
2022-09-25 07:57:59 +0700

    deploy & verify contract

--- smartcontract/hardhat.config.js
+++ smartcontract/hardhat.config.js
@@ -1,6 +1,16 @@
 require('@nomicfoundation/hardhat-toolbox')
+require('dotenv').config()
 /** @type import('hardhat/config').HardhatUserConfig */
 
 module.exports = {
   solidity: '0.8.9',
+  networks: {
+    bsctestnet: {
+      url: 'https://data-seed-prebsc-1-s1.binance.org:8545',
+      accounts: [process.env.PRIVATE_KEY],
+    },
+  },
+  etherscan: {
+    apiKey: process.env.API_KEY,
+  },
 }
